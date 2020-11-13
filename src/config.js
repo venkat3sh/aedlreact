69dcc0149c3ba8527a8599503347400c4cfe970b
Venkatesh Teja Pappula <VenkateshTeja.Pappula@anthem.com>
2020-11-13 06:40:47 -0500

    Config file added

---
+++ src/config.js
@@ -0,0 +1,19 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'us-east-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-east-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://3yxyhh7j6a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import config from './config';
 
 // Amplify.configure(awsExports);
 
