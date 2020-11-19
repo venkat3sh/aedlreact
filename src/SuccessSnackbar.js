a17e886d2676a0ce5f3ab7b6136ccd17d922b328
Venkatesh Teja Pappula <VenkateshTeja.Pappula@anthem.com>
2020-11-19 06:37:52 -0500

    Snackbar added

---
+++ src/SuccessSnackbar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+// Material UI Imports
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+function SuccessSnackbar( { open, handleSnackbarClose, responseMessage } ) {
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    handleSnackbarClose();
+  };
+
+  return (
+    <div className="snackbar-wrapper">
+      {responseMessage ? (
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <SnackbarContent 
+          style={{
+          backgroundColor:'#f44336',
+          }}
+          message={`${responseMessage}`}
+          action={
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+          />
+        </Snackbar>
+      ) : (
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open}
+          autoHideDuration={5000}
+          onClose={handleClose}
+          >
+          <SnackbarContent 
+          style={{
+            backgroundColor:'#098f3b',
+          }}
+          message={`${responseMessage}`}
+          action={
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+          />
+        </Snackbar>
+      )}
+    </div>
+  );
+}
+
+export default SuccessSnackbar
