ba65153422d2204666bf240828272ef7a07b4666
AF86384 <AF86384@LJRQ65Y2.us.ad.wellpoint.com>
2020-12-14 17:03:26 +0530

    test

--- src/SuccessSnackbar.js
+++ src/SuccessSnackbar.js
@@ -25,7 +25,7 @@ function SuccessSnackbar( { open, handleSnackbarClose, responseMessage } ) {
             horizontal: 'left',
           }}
           open={open}
-          autoHideDuration={5000}
+          autoHideDuration={5000} 
           onClose={handleClose}
         >
           <SnackbarContent 
